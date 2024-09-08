928a6d7d13e331231f8c1a7f315bb884edd6bc18
AYANscyy2 <mayan6378@gmail.com>
2024-09-08 18:53:23 +0530

    Refactored project structure: moved files to about_section and hero_section, updated page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,12 @@
 import NavBar from "./components/navbar/NavBar";
 import Timeline from "./components/timeline/Timeline";
-import { HeroMain } from "./components/navbar/hero_section/heroMain";
-import { AboutMain } from "./components/navbar/about_section/aboutMain";
 import Statistics from "./components/statistics/statistics";
 import Prizes from "./components/prizes/prizes";
 import Footer from "./components/footer/Footer";
 import Induction from "./components/induction/induction";
 import Oraganiser from "./components/organiser/organiser";
+import { HeroMain } from "./components/hero_section/heroMain";
+import { AboutMain } from "./components/about_section/aboutMain";
 
 export default function Home() {
   return (
