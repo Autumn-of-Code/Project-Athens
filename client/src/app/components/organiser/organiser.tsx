b76269831c9c454c82a1794b71e388a56d2bef13
Harsh <harshtanishq2002@gmail.com>
2024-09-09 07:48:15 +0530

    chore: moved content in config

--- client/src/app/components/organiser/organiser.tsx
+++ client/src/app/components/organiser/organiser.tsx
@@ -2,46 +2,9 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
+import { images } from "@/app/config/content/OrganiserData/OrganiserData";
 
 const Oraganiser = () => {
-  const images = [
-    {
-      src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794784/GDG_LOGO_fxtkok.png",
-      width: 500,
-      height: 300,
-      name: "GDG on campus NITR",
-      classname: "mb-5  ml-2  mt-8",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/WTM_1_kcus3p.png",
-      width: 200,
-      height: 100,
-      name: "Women Techmakers",
-      classname: "ml-12",
-    },
-    {
-      src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794313/DOC-20240821-WA0019_dgwwaq.png",
-      width: 200,
-      height: 100,
-      name: "OS Community",
-      classname: "ml-10",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725366457/Untitled_design_naamty.png",
-      width: 200,
-      height: 100,
-      name: "Tech IIIT bbsr",
-      classname: "ml-12",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/GDSC_BBSR_1_lpsskx.png",
-      width: 500,
-      height: 300,
-      name: "GDSC Silicon University",
-      classname: "mb-5  ml-2  mt-8",
-    },
-  ];
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
@@ -55,7 +18,7 @@ const Oraganiser = () => {
   };
 
   return (
-    <div className="w-100% text-primary font-jost text-3xl text-left">
+    <div className="w-100% text-[#F12727] font-jost text-3xl text-left">
       <p className="pl-12">Organisers</p>
       <div className="relative w-full max-w-6xl mx-auto overflow-hidden mt-12">
         <div
