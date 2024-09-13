56dabc67fdb1cf52a0b8c4d0f9a5878a1931647e
Harsh <harshtanishq2002@gmail.com>
2024-09-13 18:24:27 +0530

    fix: made required changes

--- client/src/app/config/content/OrganiserData/OrganiserData.ts
+++ client/src/app/config/content/OrganiserData/OrganiserData.ts
@@ -1,37 +1,37 @@
 export const images = [
-    {
-      src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794784/GDG_LOGO_fxtkok.png",
-      width: 500,
-      height: 300,
-      name: "GDG on campus NITR",
-      classname: "mb-5  ml-2  mt-8",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/WTM_1_kcus3p.png",
-      width: 200,
-      height: 100,
-      name: "Women Techmakers",
-      classname: "ml-12",
-    },
-    {
-      src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794313/DOC-20240821-WA0019_dgwwaq.png",
-      width: 200,
-      height: 100,
-      name: "OS Community",
-      classname: "ml-10",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725366457/Untitled_design_naamty.png",
-      width: 200,
-      height: 100,
-      name: "Tech IIIT bbsr",
-      classname: "ml-12",
-    },
-    {
-      src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/GDSC_BBSR_1_lpsskx.png",
-      width: 500,
-      height: 300,
-      name: "GDSC Silicon University",
-      classname: "mb-5  ml-2  mt-8",
-    },
-  ];
+  {
+    src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794784/GDG_LOGO_fxtkok.png",
+    width: 500,
+    height: 300,
+    name: "GDG on campus NITR",
+    classname: "mb-5  ml-2  mt-8",
+  },
+  {
+    src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/WTM_1_kcus3p.png",
+    width: 200,
+    height: 100,
+    name: "Women Techmakers",
+    classname: "sm:ml-20 lg:ml-12",
+  },
+  {
+    src: "https://res.cloudinary.com/dxsyc4p2j/image/upload/v1725794313/DOC-20240821-WA0019_dgwwaq.png",
+    width: 200,
+    height: 100,
+    name: "OS Community",
+    classname: "ml-10 mt-[2.5rem]",
+  },
+  {
+    src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725366457/Untitled_design_naamty.png",
+    width: 200,
+    height: 100,
+    name: "Tech IIIT bbsr",
+    classname: "ml-12",
+  },
+  {
+    src: "https://res.cloudinary.com/dqqyuvg1v/image/upload/v1725299204/GDSC_BBSR_1_lpsskx.png",
+    width: 500,
+    height: 300,
+    name: "GDSC Silicon University",
+    classname: "mb-5  ml-2  mt-8",
+  },
+];
